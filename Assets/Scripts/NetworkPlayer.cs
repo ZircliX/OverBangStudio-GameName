4e341909c0037a7d032649cb999d11fbd0b0f589
ZircliX <lolofy71@gmail.com>
2025-06-30 00:08:16 +0200

    NetCode sample

---
+++ Assets/Scripts/NetworkPlayer.cs
@@ -0,0 +1,18 @@
+using System;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class NetworkPlayer : NetworkBehaviour
+    {
+        private void Update()
+        {
+            if (IsServer)
+            {
+                float theta = Time.frameCount / 10.0f;
+                transform.position = new Vector3((float) Math.Cos(theta), 0.0f, (float) Math.Sin(theta));
+            }
+        }
+    }
+}
