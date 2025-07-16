cc962cfeb6e4ddfb0addef6ca7068fa20f95d8f3
Dydou_irl <MegaFight63@laposte.net>
2025-07-16 22:17:23 +0200

    Test #1

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,14 +1,16 @@
 using Combat.Weapon;
+using Unity.Netcode;
 using UnityEngine;
 
 namespace Player
 {
-    public class Player : MonoBehaviour
+    public class Player : NetworkBehaviour
     {
         [SerializeField] private Weapon weapon;
         [SerializeField] private Transform orientation;
         private void Update()
         {
+            if(!IsOwner) return;
             if (Input.GetMouseButtonDown(0))
             {
                 weapon.Shoot(orientation.forward);
